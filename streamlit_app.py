ba5da2a7539747e62cd00c3465181371dbb41ac1
snowflakedwcloud <119276252+snowflakedwcloud@users.noreply.github.com>
2022-11-27 20:27:18 -0800

    Update streamlit_app.py
    Added fruit name in the picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,6 +11,7 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.title('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's create a pick list to pick the fruit to be included
 streamlit.multiselect("Pick some fruit: ", list(my_fruit_list.index))
