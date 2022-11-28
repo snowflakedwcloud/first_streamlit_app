5c909421d1ab8b4d0db3d49bdbea402e7edc69a6
snowflakedwcloud <119276252+snowflakedwcloud@users.noreply.github.com>
2022-11-27 20:21:55 -0800

    Update streamlit_app.py
    Added code to extract data from s3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -6,3 +7,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Bluberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.title('Build Your Own Fruit Smoothie')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
