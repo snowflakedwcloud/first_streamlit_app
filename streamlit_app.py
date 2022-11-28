6e506bfc05839173b4528bef4cbfcec8a6152101
snowflakedwcloud <119276252+snowflakedwcloud@users.noreply.github.com>
2022-11-27 20:26:02 -0800

    Update streamlit_app.py
    Added the picker

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.title('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# Let's create a pick list to pick the fruit to be included
+streamlit.multiselect("Pick some fruit: ", list(my_fruit_list.index))
+
+# display the table on the page 
 streamlit.dataframe(my_fruit_list)
